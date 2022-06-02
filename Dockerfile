1bf995a1f80f37e8a0e3530b57b51143f041072d
kai <chongshikai@gmail.com>
2022-06-02 13:53:44 +0800

    added graph traverser

--- Dockerfile
+++ Dockerfile
@@ -11,31 +11,11 @@ ENV PYTHONDONTWRITEBYTECODE 1
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED 1
 
-# RUN apt-get update -y
-# RUN apt-get upgrade -y
-# RUN pip install --upgrade pip
-# # RUN apt-get install proj-bin -y
-# # Install pip requirements
 ADD build /build
 WORKDIR /build
 RUN make
-# RUN python -m pip install -r requirements.txt
-# RUN pip install jupyter
 
 ADD /src /src
-RUN mkdir /data
-RUN mkdir /models
-#VOLUME ["/src/files"]
-#VOLUME /src/files
-#ADD /src/files /src/files
-
-# jupyter notebook
-# RUN pip install jupyter
 
 WORKDIR /src
-# WORKDIR /src/files
-# RUN ["bash"]
-# CMD ["python3","index.py"]
-
-# CMD ["python3", "main.py"]
 # During debugging, this entry point will be overridden. For more information, refer to https://aka.ms/vscode-docker-python-debug
---
+++ src/graph_traverser.py
@@ -0,0 +1,85 @@
+
+from typing import Dict
+
+import requests
+from collections import deque
+from neo4j import GraphDatabase
+
+
+class GraphTraverser:
+
+    NEIGHBOR_QUERY = '''
+    MATCH (source:Node{entity_id :{}})
+    CALL gds.bfs.stream('myGraph', {
+    sourceNode: source,
+    maxDepth: 1
+    })
+    YIELD path
+    RETURN path
+    '''
+
+    def __init__(self):
+        self.connector = Neo4jConnection()
+
+    def get_related_entities(self, entity_list, hops):
+        results = []
+        for entity in entity_list:
+            entity_result = {entity: 0}
+            k = 0
+            queue = deque()
+            queue.append(entity)
+            while queue and k < hops:
+                k += 1
+                hop_length = len(queue)
+
+                for _ in range(hop_length):
+                    node = queue.popleft()
+                    query = self.NEIGHBOR_QUERY.format(node)
+                    neighbors = self._get_neighbor_list_from_path(
+                        self.connector(query))
+                    for neighbor in neighbors:
+                        if neighbor not in entity:
+                            entity_result[neighbor] = k
+                            queue.append(neighbor)
+
+            del entity_result[entity]
+            results.append(entity_result)
+        return results
+
+    def _get_neighbor_list_from_path(self, path):
+        return path
+
+
+class Neo4jConnection:
+
+    URI = 'bolt://localhost:7687'
+    USER = ''
+    PASS = ''
+
+    def __init__(self,):
+
+        self.__driver = None
+        try:
+            self.__driver = GraphDatabase.driver(
+                self.URI, auth=(self.USER, self.PASS))
+        except Exception as e:
+            print("Failed to create the driver:", e)
+
+    def close(self):
+        if self.__driver is not None:
+            self.__driver.close()
+
+    def query(self, query, db=None):
+        assert self.__driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try:
+            session = self.__driver.session(
+                database=db) if db is not None else self.__driver.session()
+            response = list(session.run(query))
+        except Exception as e:
+            print("Query failed:", e)
+        finally:
+            if session is not None:
+                session.close()
+        return response
